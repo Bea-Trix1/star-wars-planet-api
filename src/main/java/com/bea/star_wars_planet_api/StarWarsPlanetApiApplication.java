f2a2bea00c86facd4978737601f672143034868a
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-11-11 18:19:23 -0300

    initial commit

---
+++ src/main/java/com/bea/star_wars_planet_api/StarWarsPlanetApiApplication.java
@@ -0,0 +1,13 @@
+package com.bea.star_wars_planet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetApiApplication.class, args);
+	}
+
+}
